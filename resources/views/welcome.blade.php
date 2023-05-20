168d419904d18f4d4e9348aaa3fd43877a4384d4
alfredobarron <alfredobarronc@gmail.com>
2023-05-19 22:59:38 -0600

    add 10 second to reload and gitlab-ci

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,7 +12,11 @@
 
         <script src="https://cdn.tailwindcss.com"></script>
 
-       
+        <script>
+    setInterval(function() {
+      location.reload();
+    }, 10000);
+  </script>
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center  min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
