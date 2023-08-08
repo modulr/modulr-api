434ecccc2a0c7164d2901e4472fc4ef2e0d6e2d2
Alfredo Barron <alfredobarronc@gmail.com>
2023-08-08 17:50:31 -0600

    add FillImage to kernel

--- app/Console/Commands/FillAutopartsData.php
+++ app/Console/Commands/FillAutopartsData.php
@@ -18,7 +18,7 @@ class FillAutopartsData extends Command
      *
      * @var string
      */
-    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=50}';
+    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=800}';
 
     /**
      * The console command description.
