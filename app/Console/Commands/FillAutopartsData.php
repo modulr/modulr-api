be7c9fae323681cbb271cae0dd97c9699d202ace
Alfredo Barron <alfredobarronc@gmail.com>
2023-08-09 11:50:42 -0600

    limit down to 500

--- app/Console/Commands/FillAutopartsData.php
+++ app/Console/Commands/FillAutopartsData.php
@@ -18,7 +18,7 @@ class FillAutopartsData extends Command
      *
      * @var string
      */
-    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=800}';
+    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=500}';
 
     /**
      * The console command description.
