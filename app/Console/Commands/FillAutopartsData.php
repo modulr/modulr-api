e8d923076c13b7408076386b86b11e2cd9f9fa6b
Alfredo Barron <alfredobarronc@gmail.com>
2023-08-10 11:06:37 -0600

    5min 10 autoparts

--- app/Console/Commands/FillAutopartsData.php
+++ app/Console/Commands/FillAutopartsData.php
@@ -18,7 +18,7 @@ class FillAutopartsData extends Command
      *
      * @var string
      */
-    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=300}';
+    protected $signature = 'app:fill-autoparts-data {--skip=0} {--limit=10}';
 
     /**
      * The console command description.
