94458c1d3c4f7be91f483bfc141470f17a268f1b
alfredobarron <alfredobarronc@gmail.com>
2023-09-01 22:25:42 -0600

    minor change

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -41,7 +41,7 @@ class LoginRequest extends FormRequest
     {
         $this->ensureIsNotRateLimited();
 
-        if (! Auth::attempt($this->only('email', 'password')+ ['active' => 1], $this->boolean('remember'))) {
+        if (! Auth::attempt($this->only('email', 'password') + ['active' => 1], $this->boolean('remember'))) {
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
