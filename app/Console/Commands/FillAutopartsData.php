6535eeb28e461c24dc2399cb99cf3ebdb5024e56
Eduardo <eduardodarellano@gmail.com>
2023-11-15 12:56:35 -0600

    Pausar parrillas

--- app/Console/Commands/FillAutopartsData.php
+++ app/Console/Commands/FillAutopartsData.php
@@ -695,8 +695,8 @@ class FillAutopartsData extends Command
 
         $autoparts = DB::table('autoparts')
             ->select('id', 'ml_id', 'name', 'make_id', 'location_id','status_id')
-            ->selectRaw("CASE WHEN category_id = 2 THEN 'Salpicadera' ELSE NULL END AS Categoría")
-            ->where('category_id', 2)
+            ->selectRaw("CASE WHEN category_id = 89 THEN 'Parrilla' ELSE NULL END AS Categoría")
+            ->where('category_id', 89)
             ->where('store_id', 1)
             ->where('status_id', 1)
             ->whereNull('location_id')
