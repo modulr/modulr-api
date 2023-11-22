498616a4beed34c75a8564fa877380b34d8e979e
Eduardo <eduardodarellano@gmail.com>
2023-11-22 09:59:51 -0600

    Use Qr

--- app/Http/Controllers/LocationsController.php
+++ app/Http/Controllers/LocationsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\AutopartListLocation;
+use QrCode;
 
 class LocationsController extends Controller
 {
