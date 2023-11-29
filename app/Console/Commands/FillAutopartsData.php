c53e8458b55a12909c1395ee8a656018641d21cf
Eduardo <eduardodarellano@gmail.com>
2023-11-29 12:21:51 -0600

    Pausar reflejantes

--- app/Console/Commands/FillAutopartsData.php
+++ app/Console/Commands/FillAutopartsData.php
@@ -695,8 +695,8 @@ class FillAutopartsData extends Command
 
         $autoparts = DB::table('autoparts')
             ->select('id', 'ml_id', 'name', 'make_id', 'location_id','status_id')
-            ->selectRaw("CASE WHEN category_id = 34 THEN 'Calavera' ELSE NULL END AS Categoría")
-            ->where('category_id', 34)
+            ->selectRaw("CASE WHEN category_id = 99 THEN 'Reflejantes' ELSE NULL END AS Categoría")
+            ->where('category_id', 99)
             ->where('store_id', 1)
             ->where('status_id', 1)
             ->whereNull('location_id')
